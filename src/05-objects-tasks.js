aaa9391da97d17c8f9835482214610b4be755a51
Pawel P <p.poloch@protonmail.com>
2023-01-13 10:10:40 +0100

    fix: Added SelectorBuilder

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -118,6 +118,167 @@ function fromJSON(proto, json) {
  *  For more examples see unit tests.
  */
 
+// class Selector {
+//   constructor(value) {
+//     this.value = value;
+//   }
+//
+//   stringify() {
+//     return this.value;
+//   }
+// }
+//
+// class ElementSelector extends Selector {
+// }
+//
+// class IdSelector extends Selector {
+//   stringify() {
+//     return `#${this.value}`;
+//   }
+// }
+//
+// class ClassSelector extends Selector {
+//   stringify() {
+//     return `.${this.value}`;
+//   }
+// }
+//
+// class AttrSelector extends Selector {
+//   stringify() {
+//     return `[${this.value}]`;
+//   }
+// }
+//
+// class PseudoClassSelector extends Selector {
+//   stringify() {
+//     return `:${this.value}`;
+//   }
+// }
+//
+// class PseudoElementSelector extends Selector {
+//   stringify() {
+//     return `::${this.value}`;
+//   }
+// }
+//
+// class _Combinator extends Selector {
+//   stringify() {
+//     // return this.value !== ' ' ? ` ${this.value} ` : this.value;
+//     return ` ${this.value} `;
+//   }
+// }
+//
+// class SelectorBuilder {
+//   constructor(...selectors) {
+//     SelectorBuilder.checkOrder(selectors);
+//
+//
+//     this.selectors = selectors;
+//   }
+//
+//   static checkOrder(selectors) {
+//     const order = [
+//       'ElementSelector',
+//       'IdSelector',
+//       'ClassSelector',
+//       'AttrSelector',
+//       'PseudoClassSelector',
+//       'PseudoElementSelector',
+//     ];
+//
+//     const classNames = selectors.map((sel) => sel.constructor.name);
+//
+//     if (classNames.includes('_Combinator')) {
+//       return;
+//     }
+//
+//     const indexes = classNames.map((cName) => order.indexOf(cName));
+//
+//     for (let i = 0; i < indexes.length - 1; i += 1) {
+//       if (indexes[i] > indexes[i + 1]) {
+//         throw new Error('Selector parts should be arranged in the following order: ' +
+//           'element, id, class, attribute, pseudo-class, pseudo-element');
+//       }
+//     }
+//   }
+//
+//   static element(value) {
+//     return new SelectorBuilder(new ElementSelector(value));
+//   }
+//
+//   element(value) {
+//     if (this.selectors.some((sel) => (sel instanceof ElementSelector))) {
+//       throw new Error('Element, id and pseudo-element ' +
+//         'should not occur more then one time inside the selector');
+//     }
+//     return new SelectorBuilder(...this.selectors, new ElementSelector(value));
+//   }
+//
+//   static id(value) {
+//     return new SelectorBuilder(new IdSelector(value));
+//   }
+//
+//   id(value) {
+//     if (this.selectors.some((sel) => (sel instanceof IdSelector))) {
+//       throw new Error('Element, id and pseudo-element ' +
+//         'should not occur more then one time inside the selector');
+//     }
+//     return new SelectorBuilder(...this.selectors, new IdSelector(value));
+//   }
+//
+//   static class(value) {
+//     return new SelectorBuilder(new ClassSelector(value));
+//   }
+//
+//   class(value) {
+//     return new SelectorBuilder(...this.selectors, new ClassSelector(value));
+//   }
+//
+//   static pseudoElement(value) {
+//     return new SelectorBuilder(new PseudoElementSelector(value));
+//   }
+//
+//   pseudoElement(value) {
+//     if (this.selectors.some((sel) => (sel instanceof PseudoElementSelector))) {
+//       throw new Error('Element, id and pseudo-element should not ' +
+//         'occur more then one time inside the selector');
+//     }
+//     return new SelectorBuilder(...this.selectors, new PseudoElementSelector(value));
+//   }
+//
+//   static pseudoClass(value) {
+//     return new SelectorBuilder(new PseudoClassSelector(value));
+//   }
+//
+//   pseudoClass(value) {
+//     return new SelectorBuilder(...this.selectors, new PseudoClassSelector(value));
+//   }
+//
+//   static attr(value) {
+//     return new SelectorBuilder(new AttrSelector(value));
+//   }
+//
+//   attr(value) {
+//     return new SelectorBuilder(...this.selectors, new AttrSelector(value));
+//   }
+//
+//   static combine(selector1, combinator, selector2) {
+//     return new SelectorBuilder(
+//       ...selector1.selectors,
+//       new _Combinator(combinator),
+//       ...selector2.selectors,
+//     );
+//   }
+//
+//   stringify() {
+//     return this.selectors.map((sel) => sel.stringify())
+//       .join('');
+//   }
+// }
+//
+//
+// const cssSelectorBuilder = SelectorBuilder;
+
 const cssSelectorBuilder = {
   element(/* value */) {
     throw new Error('Not implemented');
